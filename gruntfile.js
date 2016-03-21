4d98a8b3929525fac8af92eab9e19a0f2d9fe194
witzawitz <witzawitz@gmail.com>
2016-03-21 18:07:27 +0300

    Added SASS (why not).

--- gruntfile.js
+++ gruntfile.js
@@ -1,63 +1,89 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg   : grunt.file.readJSON('package.json'),
 		clean : {
-			options : {
-				force : true
+			options   : {
+				force: true
 			},
 			production: [
 				'dist/**/*'
 			]
 		},
-		concat : {
-			js : {
+		concat: {
+			js: {
 				src : [
 					'source/js/<%= pkg.name %>.js'
 				],
-				dest : 'dist/js/<%= pkg.name %>.js'
+				dest: 'dist/js/<%= pkg.name %>.js'
 			}
 		},
-		less: {
-			development : {
-				options : {
-					"paths": [
-					  "bower_components"
+		less  : {
+			development: {
+				options: {
+					"paths"    : [
+						"bower_components"
 					],
-					cleancss: true,
+					cleancss   : true,
 					strictUnits: true
 				},
-				files: { 'dist/css/<%= pkg.name %>.css': 'source/less/<%= pkg.name %>.less' }
+				files  : { 'dist/css/<%= pkg.name %>.css': 'source/less/<%= pkg.name %>.less' }
 			},
 			production : {
-				options : {
-					"paths": [
-					  "bower_components"
+				options: {
+					"paths"     : [
+						"bower_components"
 					],
-					compress: true,
-					yuicompress: true,
-					strictUnits: true,
-					optimization: 2
+					compress    : true,
+					yuicompress : true,
+					strictUnits : true,
+					optimization: 2,
+					sourceMap   : true
 				},
-				files: { 'dist/css/<%= pkg.name %>.min.css': 'source/less/<%= pkg.name %>.less' }
+				files  : { 'dist/css/<%= pkg.name %>.min.css': 'source/less/<%= pkg.name %>.less' }
+			}
+		},
+		sass  : {
+			development: {
+				options: {
+					precision: 3,
+					style    : 'expanded',
+					sourcemap: 'none'
+				},
+				files  : {
+					'dist/css/<%= pkg.name %>.css': 'source/sass/<%= pkg.name %>.scss'
+				}
+			},
+			production : {
+				options: {
+					precision: 3,
+					style    : 'compressed'
+				},
+				files  : {
+					'dist/css/<%= pkg.name %>.min.css': 'source/sass/<%= pkg.name %>.scss'
+				}
 			}
 		},
 		watch : {
-			less : {
-				files : [ 'source/less/**/*' ],
-				tasks : [ 'less:development' ]
+			less  : {
+				files: ['source/less/**/*'],
+				tasks: ['less:development']
 			},
-			js : {
-				files: [ 'source/js/**/*.js' ],
-				tasks: [ 'concat' ]
+			sass  : {
+				files: ['source/sass/**/*'],
+				tasks: ['sass:development']
 			},
-			uglify : {
-				files : [ '<%= uglify.js.src %>' ],
-				tasks : [ 'uglify:js' ]
+			js    : {
+				files: ['source/js/**/*.js'],
+				tasks: ['concat']
+			},
+			uglify: {
+				files: ['<%= uglify.js.src %>'],
+				tasks: ['uglify:js']
 			}
 		},
-		uglify : {
+		uglify: {
 			js: {
-				src: 'dist/js/<%= pkg.name %>.js',
+				src : 'dist/js/<%= pkg.name %>.js',
 				dest: 'dist/js/<%= pkg.name %>.min.js'
 			}
 		}
@@ -69,6 +95,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', [ 'less', 'concat', 'uglify', 'watch' ]);
-	grunt.registerTask('production', [ 'clean', 'less', 'concat', 'uglify' ]);
+	grunt.registerTask('default', ['less', 'concat', 'uglify', 'watch']);
+	grunt.registerTask('production', ['clean', 'less', 'concat', 'uglify']);
 };
