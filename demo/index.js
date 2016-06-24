8f6e7cbffd3aaecb5a64e5867a7c217a0c18c1aa
adinvadim <adin.vadim@gmail.com>
2016-06-24 18:27:45 +0300

    Added demo with controls in one line

--- demo/index.js
+++ demo/index.js
@@ -5,4 +5,22 @@ $(function() {
 		var name = $(this).data('name');
 		theme.attr('href', '../dist/css/' + name + '/le-player.css');
 	})
+
+
+	//var _toggled = false;
+	$('.btn-hide-divider').on('click', function(e) {
+		var id = $(e.target).attr('data-video-id');
+		var container = $('#'+id).parents('.leplayer-container');
+		//var controls = container.find('.leplayer-controls.controls-common');
+		//if (_toggled) {
+			//controls.css({
+				//'margin-top' : '10px'
+			//})
+		//} else {
+			//controls.css({
+				//'margin-top' : '0'
+			//})
+		//}
+		//_toggled = true;
+	})
 })
