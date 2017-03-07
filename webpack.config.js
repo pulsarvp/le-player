5be5491ecfcee15173e157d0dfa30d0eb8345c65
adinvadim <adin.vadim@gmail.com>
2017-03-07 15:32:35 +0300

    [#LPLR-212]: Add raw-loader; spaces -> tabs

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,55 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        'le-player' : './source/js/le-player.js',
-        'le-player-ga' : './source/js/plugins/le-player-ga.js'
-    },
+	entry: {
+		'le-player' : './source/js/le-player.js',
+		'le-player-ga' : './source/js/plugins/le-player-ga.js'
+	},
 
-    output: {
-        path: path.join(__dirname, 'dist', 'js'),
-        filename: '[name].js',
-    },
+	output: {
+		path: path.join(__dirname, 'dist', 'js'),
+		filename: '[name].js',
+		publicPath: "/dist/js/"
+	},
 
-    module: {
+	module: {
 		preLoaders : [
 			{
 				test: /(\.js)$/,
 				loader : 'eslint',
-                exclude: /(node_modules|bower_components|jquery)/,
-                include: path.join(__dirname, 'source', 'js'),
+				exclude: /(node_modules|bower_components|jquery)/,
+				include: path.join(__dirname, 'source', 'js'),
 			}
 		],
-        loaders: [
-            {
-                test: /(\.js)$/,
-                loader: 'babel',
-                exclude: /(node_modules|bower_components|jquery)/,
-                include: path.join(__dirname, 'source', 'js'),
-                query: {
-                    presets: ['es2015', 'stage-0']
-                }
-            }
-        ]
-    },
+		loaders: [
+			{
+				test: /(\.js)$/,
+				loader: 'babel',
+				exclude: /(node_modules|bower_components|jquery)/,
+				include: path.join(__dirname, 'source', 'js'),
+				query: {
+					presets: ['es2015', 'stage-0']
+				}
+			},
+			{
+				test: /(\.svg)$/,
+				loader: 'raw-loader',
+				exclude: /(node_modules|bower_components|jquery)/,
+				include: path.join(__dirname, 'dist', 'svg'),
+			},
+		]
+	},
 
-    resolve: {
-        modulesDirectories: ['node_modules', 'bower_components'],
-        alias: {
-          // For libs from node_modules or bower_components
-          //'jquery': 'jquery/dist/jquery.min'
-        }
-    },
-    externals: {
-        jquery: "$"
-    },
+	resolve: {
+		modulesDirectories: ['node_modules', 'bower_components'],
+		alias: {
+		  // For libs from node_modules or bower_components
+		  //'jquery': 'jquery/dist/jquery.min'
+		}
+	},
+	externals: {
+		jquery: "$"
+	},
 	eslint : {
 		emitWarning: true
 	}
