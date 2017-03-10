2c19ea49e24c1a06dfedfaf9dc8d4eeebefb1629
adinvadim <adin.vadim@gmail.com>
2017-03-10 16:17:25 +0300

    [#LPLR-225]: Support loop and autoplay settings

--- source/js/le-player.js
+++ source/js/le-player.js
@@ -310,6 +310,7 @@ class Player extends Component {
 			 */
 			this.trigger('sectionsinit', data);
 		});
+		this._initPlugins();
 
 		this.video.init().then(() => {
 			/**
@@ -318,9 +319,11 @@ class Player extends Component {
 			 * @event Player#inited
 			 */
 			this.trigger('inited');
-			this._initPlugins();
-		});
 
+			if(this.video.src != null && this.options.autoplay) {
+				this.play();
+			}
+		});
 
 		this._listenHotKeys();
 
