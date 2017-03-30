8317db233334d75d0378aea3db93d3bb94acfe9a
adinvadim <adin.vadim@gmail.com>
2017-03-30 19:51:44 +0300

    [#LPLR-219]: Next video plugin

--- source/js/plugins/le-player-next.js
+++ source/js/plugins/le-player-next.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 const Player = window.lePlayer;
 const Component = Player.getComponent('Component');
 const Icon = Player.getComponent('Icon');
-const PlayButton = Player.getComponent('PlayButton');
+const secondsToTime = Player.secondsToTime;
 
 class RadialBar extends Component {
 
@@ -14,24 +14,24 @@ class RadialBar extends Component {
 		}, options);
 		super(player, options);
 		this._circleElement = this.element.find('svg circle');
-		this.element.on('click', () => this.trigger('click'));
 	}
 
 	createElement() {
 		const { strokeDasharray, size } = this.options;
 		const template = `
-			<div class="radial-bar leplayer-play-button">
-				<svg height="100%" version="1.1" viewBox="0 0 ${size} ${size}" class="radial-bar__svg-spinner">
-				<circle cx="${-size / 2}" cy="${size / 2}" fill-opacity="0" r="46.5" stroke="currentColor" stroke-dasharray="${strokeDasharray}" stroke-dashoffset="${-1 * strokeDasharray}" stroke-width="6" transform="rotate(-90)">
-				</circle>
-				</svg>
-				<div class="radial-bar__button leplayer-play-button__button"></div>
+			<div class="leplayer-next__play leplayer-play-button">
+                <svg height="100%" version="1.1" viewBox="0 0 ${size} ${size}" class="leplayer-next__svg-spinner">
+                    <circle cx="${-size / 2}" cy="${size / 2}" fill-opacity="0" r="46.5" stroke="currentColor" stroke-dasharray="${strokeDasharray}" stroke-dashoffset="${-1 * strokeDasharray}" stroke-width="6" transform="rotate(-90)">
+                    </circle>
+                </svg>
+				<div class="leplayer-next__button leplayer-play-button__button">
+				</div>
 			</div>
 		`.trim()
 		this.element = $('<div />').html(template).contents();
 
 		this.element
-			.find('.radial-bar__button')
+			.find('.leplayer-next__button')
 			.append(new Icon(this.player, { iconName : 'play' }).element);
 
 		return this.element;
@@ -69,9 +69,7 @@ class NextComponent extends Component {
 			title : '',
 			description : '',
 			timeout : 5000,
-
 		}, options);
-		console.log('in constructor', options);
 
 		super(player, options);
 
@@ -87,26 +85,26 @@ class NextComponent extends Component {
 			}
 		})
 
-		this.progressButton.on('click', this.next.bind(this));
+		this.progressButton.element.on('click', this.next.bind(this));
 		this.cancelButton.on('click', this.cancel.bind(this));
 
 	}
 
 	createElement() {
-		const { timeout, title, description, poster, auto } = this.options;
-		console.log(poster);
+		const { timeout, title, description, poster, auto, time } = this.options;
 		const template = `
 			<div class="leplayer-next">
-				<div class="leplayer-next__header">
-					<div class="leplayer-next__head">Следующее видео</div>
-					<div class="leplayer-next__title">${title}</div>
-					<div class="leplayer-next__description">${description}</div>
-				</div>
-
-				<span class="_button-replace"></span>
-
-				<div class="leplayer-next__footer">
-					<div class="leplayer-next__cancel" role="button">Отмена</div>
+                <div class="leplayer-next__head">Следующее видео</div>
+				<div class="leplayer-next__box">
+					<div class="leplayer-next__poster">
+						<div class="leplayer-next__mini-title">${title}</div>
+						<div class="leplayer-next__cancel" role="button">Отмена</div>
+					</div>
+					<div class="leplayer-next__content">
+                        <div class="leplayer-next__title">${title}</div>
+                        <div class="leplayer-next__description">${description}</div>
+                        ${time ? `<div class="leplayer-next__time">${secondsToTime(time)}</div>` : ''}
+					</div>
 				</div>
 			</div>
 
@@ -117,13 +115,16 @@ class NextComponent extends Component {
 		this.element = $('<div />').html(template).contents();
 
 		this.element
-			.css({
-				'background-image' : `url(${poster})`
-			});
+			.find('.leplayer-next__poster')
+			.append(this.progressButton.element);
 
-		this.element
-			.find('._button-replace')
-			.replaceWith(this.progressButton.element);
+		if(poster != null) {
+			this.element
+				.find('.leplayer-next__poster')
+				.css({
+					'background-image' : `url(${poster})`
+				});
+		}
 
 		if(auto) {
 			this.addClass('leplayer-next--auto');
@@ -168,7 +169,6 @@ class NextComponent extends Component {
 
 
 Player.plugin('next', function(pluginOptions) {
-	console.log(pluginOptions);
 	this._nextComponent = new NextComponent(this, pluginOptions)
 
 	this.innerElement.append(this._nextComponent.element);
