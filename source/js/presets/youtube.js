376a32d32399bfcac52a5e6e054e73b2016f5131
adinvadim <adin.vadim@gmail.com>
2017-04-04 14:55:39 +0300

    [#LPLR-241]: Youtube preset

---
+++ source/js/presets/youtube.js
@@ -0,0 +1,14 @@
+export const preset = {
+	options : {
+		entity : 'Youtube',
+		controls : {
+			common : [
+				['play', 'volume', 'timeline', 'rate', 'divider', 'fullscreen'],
+				[]
+			],
+			fullscreen : [
+				['play', 'volume', 'timeline', 'rate', 'divider', 'fullscreen'],
+			]
+		}
+	},
+}
