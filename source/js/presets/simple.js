82e52121f7ced92d1da2d1051f5eb69451b6d482
adinvadim <adin.vadim@gmail.com>
2017-07-20 17:57:34 +0300

    [LPLR-274]: New preset: simple

---
+++ source/js/presets/simple.js
@@ -0,0 +1,15 @@
+const controls = ['play', 'volume', 'timeline', 'rate', 'download', 'fullscreen'];
+
+export const preset = {
+	options : {
+		controls : {
+			common : [
+				controls,
+				[]
+			],
+			fullscreen : [
+				controls
+			]
+		}
+	},
+}
