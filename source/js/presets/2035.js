5330d8d0fc34f9aab66e0a2ac01d9c8c655fa354
lea_alex <lea.alexx@gmail.com>
2019-05-23 23:22:22 +0500

    новая версия дизайна плеера

--- source/js/presets/2035.js
+++ source/js/presets/2035.js
@@ -1,10 +1,15 @@
 export const preset = {
 	options : {
 		width : '100%',
+
+		controlsOptions : {
+			common : {
+				align : ['justify']
+			}
+		},
 		controls : {
 			common : [
 				['play', 'timeline', 'rate', 'volume', 'source', 'fullscreen'],
-				[]
 			],
 			fullscreen : [
 				['play', 'timeline', 'rate', 'volume', 'source', 'fullscreen']
